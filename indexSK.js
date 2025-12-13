7a113cd3526df39b9490a802fe4cdedb3d44d62e
TMTien08 <160707667+TMTien08@users.noreply.github.com>
2025-12-13 09:57:30 +0700

    update
    Added functions to filter events by date and name.

--- indexSK.js
+++ indexSK.js
@@ -42,4 +42,68 @@ function xoaSK(id){
     }
 }
 
+function hienThiSKTheoNgay(){
+    var ngay = $('#ngay').val();
+    $.ajax({
+        url: 'https://692b135b7615a15ff24ea9d3.mockapi.io/events',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data){
+            var DSSK = '';
+            data.forEach(function(event){
+                if (event.date == ngay){
+                    DSSK += `
+                    <tr>
+                        <td>${event.id}</td>
+                        <td>${event.title}</td>
+                        <td>${event.location}</td>
+                        <td>${event.date}</td>
+                        <td>${event.time}</td>
+                        <td>${event.organizer}</td>
+                        <td>${event.description}</td>
+                        <td>${event.createAt}</td>
+                        
+                        <td><a class="btn btn-warning" href="sua?id=${event.id}">Sửa</a></td>
+                        <td><button class="btn btn-danger" onclick=xoaSK(${event.id})">Xóa</button></td>
+                    </tr>
+                    `;
+                }
+            });
+            $('#danhSachSK').html(html);
+        }
+    });
+}
+
+function hienThiSKTheoTen(){
+    var ten = $('#ten').val();
+    $.ajax({
+        url: 'https://692b135b7615a15ff24ea9d3.mockapi.io/events',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data){
+            var DSSK = '';
+            data.forEach(function(event){
+                if (event.title == ten){
+                    DSSK += `
+                    <tr>
+                        <td>${event.id}</td>
+                        <td>${event.title}</td>
+                        <td>${event.location}</td>
+                        <td>${event.date}</td>
+                        <td>${event.time}</td>
+                        <td>${event.organizer}</td>
+                        <td>${event.description}</td>
+                        <td>${event.createAt}</td>
+                        
+                        <td><a class="btn btn-warning" href="sua?id=${event.id}">Sửa</a></td>
+                        <td><button class="btn btn-danger" onclick=xoaSK(${event.id})">Xóa</button></td>
+                    </tr>
+                    `;
+                }
+            });
+            $('#danhSachSK').html(html);
+        }
+    });
+}
+
 
